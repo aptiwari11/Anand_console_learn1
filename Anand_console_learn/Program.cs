6fac34f2d627252a7d26e0c41ced7affc7e5bf66
Prakash <anand.prakash@epicor.com>
2018-05-13 00:54:29 +0530

    Add project files.

---
+++ Anand_console_learn/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anand_console_learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
